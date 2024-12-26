b319a2dff177867c918cdeb35d7a781501ae0538
Kagean <egekaan.kc3@gmail.com>
2024-12-26 07:11:50 +0300

    Fire Bug Fix
    The issue of hitting enemies outside the screen and bullets traveling infinitely has been resolved by placing colliders with rigidbodies outside the screen.

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -14,15 +14,14 @@ public class Destroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < 17.0f && !canBeDestroyed)
-        {
+
             canBeDestroyed = true;
             Attack[] attacks = transform.GetComponentsInChildren<Attack>();
             foreach (Attack attack in attacks)
             {
                 attack.isActive = true;
             }
-        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
