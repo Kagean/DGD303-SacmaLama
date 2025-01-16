5c56a94f2cd36c3672e51b2b7a29a9c9bbe96ae0
Ege Kaan <215040030@stu.istinye.edu.tr>
2025-01-16 20:10:43 +0300

    Main Menu almost Finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,13 +9,11 @@ namespace Shmup
     {
         [SerializeField] SceneReference startingLevel;
         [SerializeField] Button playButton;
-        [SerializeField] Button SettingsButton;
         [SerializeField] Button quitButton;
 
         void Awake()
         {
             playButton.onClick.AddListener(() => Loader.Loading(startingLevel));
-            SettingsButton.onClick.AddListener(() => Helpers.QuitGame());
             quitButton.onClick.AddListener(() => Helpers.QuitGame());
             Time.timeScale = 1f;
         }
